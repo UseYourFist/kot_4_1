4b8e39632fef801f0d9b783953e475457d3e3abf
UseYourFist <spr47@mail.ru>
2022-04-16 18:22:10 +0700

    bad commit

---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,78 @@
+package ru.netology
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun commissionCalculation_Mastercard_Maestro_Sumall_smaller75() {
+        val card = "Mastercard"
+        val all = 0
+        val now = 80_000_00
+
+        val result = commissionCalculation(
+            cardType = card,
+            sumAll = all,
+            sumNow = now
+        )
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun commissionCalculation_Mastercard_Maestro_Sumall_more75() {
+        val card = "Mastercard"
+        val all = 75_000_00
+        val now = 80_000_00
+
+        val result = commissionCalculation(
+            cardType = card,
+            sumAll = all,
+            sumNow = now
+        )
+        assertEquals(482000, result)
+    }
+
+    @Test
+    fun commissionCalculation_VKPay() {
+        val card = "VKPay"
+        val all = 0
+        val now = 80_000_00
+
+        val result = commissionCalculation(
+            cardType = card,
+            sumAll = all,
+            sumNow = now
+        )
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionCalculation_Visa_Mir_comission_smaller35() {
+        val card = "Visa"
+        val all = 0
+        val now = 80
+
+        val result = commissionCalculation(
+            cardType = card,
+            sumAll = all,
+            sumNow = now
+        )
+        assertEquals(3500, result)
+    }
+
+    @Test
+    fun commissionCalculation_Visa_Mir_comission_more35() {
+        val card = "Visa"
+        val all = 0
+        val now = 80_000_00
+
+        val result = commissionCalculation(
+            cardType = card,
+            sumAll = all,
+            sumNow = now
+        )
+        assertEquals(600000, result)
+    }
+}
